f9b4b4de563ce2edef6b20295a062495cf6c29e6
rohitsinghudi1372008-crypto <rohitsinghudi1372008-crypto@users.noreply.github.com>
2026-08-19 14:06:37 +0530

    okk

---
+++ Day3/A.js
@@ -0,0 +1,87 @@
+// console.log("=== Array ===");
+
+// var fruits = ["Apple", "Banana", "Orange"];
+// console.log("Fruits: " + fruits);
+
+// var numbers = [1, 2, 3, 4, 5];
+// console.log("Numbers: " + numbers);
+
+// //---------------------------------------
+
+// console.log("==string Array==");
+// var names = ["John", "Jane", "Mike"];
+// console.log("Names: " + names);
+
+// //---------------------------------------
+
+// console.log("=== String methods ===");
+
+// let str = "Hello, World!";
+// console.log("Length of string: " + str.length);
+
+// //1. toUpperCase() - Converts the string to uppercase letters.
+// console.log("Uppercase: " + str.toUpperCase());
+
+// //2. toLowerCase() - Converts the string to lowercase letters.
+// console.log("Lowercase: " + str.toLowerCase());
+
+// //3. charAt() - Returns the character at a specified index.
+// console.log("Character at index 7: " + str.charAt(7));
+
+// //4. substring() - Extracts a part of a string between two indices.
+// console.log("Substring (0, 5): " + str.substring(0, 5));
+
+// //5. indexOf() - Returns the position of the first occurrence of a specified value.
+// console.log("Index of 'World': " + str.indexOf("World"));
+// let s="Rohit singh"
+// console.log(s.length)
+// let arr=[1,3,4];
+// console.log(arr.push(2)) 
+// console.log(arr.slice(0,2))
+// console.log(s.toUpperCase());
+// console.log(s.toLowerCase())
+// console.log(s.slice(1,3))
+// console.log("Sarthak",s.indexOf("Sarthak"))
+// console.log("utkarsh",s.lastIndexOf("utkarsh"))
+
+
+// //slice
+// let str1 = "Hello, World!";
+// console.log("Slice (7, 12): " + str1.slice(7, 12));
+
+// //split
+// let str2 = "Hello, World!";
+// let arr2 = str2.split(", ");
+// console.log("Split: " + arr2);
+
+// //replace
+// let str3 = "Hello, World!";
+// let newStr = str3.replace("World", "JavaScript");
+// console.log("Replace: " + newStr);
+
+// //concat
+// let str4 = "Hello";
+// let str5 = "World";
+// let concatenatedStr = str4.concat(", ", str5, "!");
+// console.log("Concatenated String: " + concatenatedStr);
+// //includes
+// let str6 = "Hello, World!";
+// console.log("Includes 'World': " + str6.includes("World"));
+// //Array object
+// let arr1 = [1, 2, 3, 4, 5];
+// console.log("Array: " + arr1);
+// //array methods
+// //push
+// arr1.push(6);
+// console.log("Array after push: " + arr1);
+// //shift
+// arr1.shift();
+// console.log("Array after shift: " + arr1);
+// //unshift
+// arr1.unshift(0);
+// console.log("Array after unshift: " + arr1);
+//date object
+let date = new Date();
+console.log("Current Date: " + date);
+//getFullYear
+console.log("Current Year: " + date.getFullYear());
